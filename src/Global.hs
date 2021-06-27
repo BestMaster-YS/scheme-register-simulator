0f2b1b830130efa39bb9157c52f8ca8fd0c8f505
BestMasterLiubin <bestmasterliubin@gmail.com>
2021-06-27 23:23:24 +0800

    feat: lib functions

--- src/Global.hs
+++ src/Global.hs
@@ -1,7 +1,12 @@
 module Global where
 
+import System.IO.Unsafe
 import qualified Data.Map as Map
 import Instruction ( InstValue(..) )
+import Language.IO ( readExp, unsafePrompt )
+import Language.Syntax
+import Language.Parser
+import Parser
 
 globalOperatorFunctions :: Map.Map String ([InstValue] -> InstValue)
 globalOperatorFunctions = Map.empty
@@ -44,12 +49,43 @@ remInst is = if length is == 2
                 _ -> error ("\"+\" expcet two Integer arguments but actally is " ++ show i)
             else error "\"+\" need two arguments"
 
+readSchemeExp :: [InstValue] -> InstValue
+readSchemeExp _ =
+  let exp = unsafePerformIO (readExp "")
+  in IString exp
+
+normalPrint :: [InstValue] -> InstValue
+normalPrint (v:_) =
+  let _ = unsafePrompt (show v)
+  in INull
+
+schemeSyntaxpredicate :: (SchemeContent -> Bool) -> [InstValue] -> InstValue
+schemeSyntaxpredicate p (v:_) =
+  case v of
+    IString s ->
+      case runParser parseScheme s of
+        -- readExp 已经进行了错误处理，这里就不重复处理
+        ParseResult a _ -> if p a then IBool True else IBool False
+    _ -> error "scheme syntax runtime error"
+
 
 baseFunctions :: [(String, [InstValue] -> InstValue)]
 baseFunctions = [
   ("+", \intses -> if length intses <= 1 then error "\"+\" need two arguments at least" else plus intses),
   ("=", eqInst),
-  ("rem", remInst)
+  ("rem", remInst),
+  ("read", readSchemeExp),
+  ("prompt-for-input", normalPrint),
+  ("user-print", normalPrint),
+  ("self-evaluating?", schemeSyntaxpredicate selfEvaluating),
+  ("variable?", schemeSyntaxpredicate isVarible),
+  ("quoted?", schemeSyntaxpredicate isSymbol),
+  ("assignment?", schemeSyntaxpredicate isAssignment),
+  ("definition?", schemeSyntaxpredicate isDefinition),
+  ("if?", schemeSyntaxpredicate isIf),
+  ("lambda?", schemeSyntaxpredicate isLambda),
+  ("begin?", schemeSyntaxpredicate isBegin),
+  ("application?", schemeSyntaxpredicate isApplication)
   ]
 
 initBaseFns :: Map.Map String ([InstValue] -> InstValue)
--- src/Language/Syntax.hs
+++ src/Language/Syntax.hs
@@ -17,7 +17,43 @@ data SchemeContent = SDouble Double
                   | SUnknownExpression
                   deriving Show
 
+selfEvaluating :: SchemeContent -> Bool
+selfEvaluating (SDouble _) = True
+selfEvaluating (SInteger _) = True
+selfEvaluating (SString _) = True
+selfEvaluating (SBool _) = True
+selfEvaluating _ = False
 
+isVarible :: SchemeContent -> Bool
+isVarible (SVarible _) = True
+isVarible _ = False
 
+isSymbol :: SchemeContent -> Bool
+isSymbol (SSymbol _) = True
+isSymbol _ = False
+
+isAssignment :: SchemeContent -> Bool
+isAssignment (SAssignmentExpression _ _) = True
+isAssignment _ = False
+
+isDefinition :: SchemeContent -> Bool
+isDefinition (SDefinitionExpreaaion _ _) = True
+isDefinition _ = False
+
+isIf :: SchemeContent -> Bool
+isIf SIfExpression {} = True
+isIf _ = False 
+
+isLambda :: SchemeContent -> Bool
+isLambda (SLambdaExpression _ _) = True
+isLambda _ = False
+
+isBegin :: SchemeContent -> Bool
+isBegin (SBeginExpression _) = True
+isBegin _ = False
+
+isApplication :: SchemeContent -> Bool
+isApplication (SApplicationExpression _ _) = True
+isApplication _ = False
 
 
